0ea41b31f2f9786dec7b110e7dffb4c51eaf5db3
Larry Adames <adames.larry@gmail.com>
2023-08-15 16:29:35 -0400

    PLT-7034 set up project - create landing page and payouts routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
+// App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Landing from './Landing';
+import Payouts from './Payouts';
 
-function App() {
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/payouts" element={<Payouts />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
+// 1) Import React and React Dom libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/main.scss';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//  2) Get a reference to the div with ID root
+const el = document.getElementById('root');
+
+if (!el) {
+  throw new Error('Root element not found');
+}
+
+//  3) Tell React to take control of that element
+const root = ReactDOM.createRoot(el);
+
+// 4) Show the component on the screen
+root.render(<App />);
