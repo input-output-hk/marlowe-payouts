c80a5b03febabb5d51bc77a4f01c5c678b4ae09f
Larry Adames <adames.larry@gmail.com>
2023-08-16 10:14:08 -0400

    PLT-7034 add payouts test data to sdk

--- src/components/Payouts.tsx
+++ src/components/Payouts.tsx
@@ -39,45 +39,6 @@ const Payouts: React.FC<PayoutsProps> = ({sdk}) => {
     navigate('/');
   }
 
-  const tokens1 = [
-    { id: 1, tokenName: "TokenA", currencySymbol: "TA", amount: 100 },
-    { id: 2, tokenName: "TokenB", currencySymbol: "TB", amount: 200 }
-  ];
-
-  const tokens2 = [
-    { id: 3, tokenName: "TokenC", currencySymbol: "TC", amount: 300 },
-    { id: 4, tokenName: "TokenD", currencySymbol: "TD", amount: 400 }
-  ];
-
-  const payout1 = {
-    id: 1,
-    name: "Payout1",
-    iconImage: "/path/to/payout1-icon.jpg",
-    roleToken: "RoleToken1",
-    actionLabel: "Withdraw",
-    tokens: [tokens1, tokens2]
-  };
-
-  const payout2 = {
-    id: 2,
-    name: "Payout2",
-    iconImage: "/path/to/payout2-icon.jpg",
-    roleToken: "RoleToken2",
-    actionLabel: "Withdraw",
-    tokens: [tokens1, tokens2]
-  };
-
-  const payoutBundle = {
-    id: 5,
-    name: "PayoutBundle1",
-    iconImage: "/path/to/payout-bundle-icon.jpg",
-    roleToken: "RoleTokenBundle",
-    actionLabel: "Withdraw Bundle",
-    payouts: [payout1, payout2],
-    tokens: []
-  };
-
-  const payouts = [payout1, payout2, payoutBundle];
 
   return (
     <div className="container">
--- src/services/MarloweSDK.ts
+++ src/services/MarloweSDK.ts
@@ -8,6 +8,7 @@ class MarloweSDK {
   connectedWallet: Wallet | null;
   lovelaceBalance: number;
   changeAddress: string | null;
+  payouts: any[];
 
   constructor() {
     this.validWalletNames = [
@@ -21,6 +22,42 @@ class MarloweSDK {
     this.connectedWallet = null;
     this.lovelaceBalance = 125000000;
     this.changeAddress = null;
+    const tokens1 = [
+      { id: 1, tokenName: "TokenA", currencySymbol: "TA", amount: 100 },
+      { id: 2, tokenName: "TokenB", currencySymbol: "TB", amount: 200 }
+    ];
+
+    const tokens2 = [
+      { id: 3, tokenName: "TokenC", currencySymbol: "TC", amount: 300 },
+      { id: 4, tokenName: "TokenD", currencySymbol: "TD", amount: 400 }
+    ];
+
+    const payout1 = {
+      id: 1,
+      name: "Payout1",
+      iconImage: "/path/to/payout1-icon.jpg",
+      roleToken: "RoleToken1",
+      actionLabel: "Withdraw",
+      tokens: [tokens1, tokens2]
+    };
+
+    const payout2 = {
+      id: 2,
+      name: "Payout2",
+      iconImage: "/path/to/payout2-icon.jpg",
+      roleToken: "RoleToken2",
+      actionLabel: "Withdraw",
+      tokens: [tokens1, tokens2]
+    };
+
+    const payouts = [payout1, payout2];
+
+    this.payouts = payouts;
+
+  }
+
+  getPayouts() {
+    return this.payouts;
   }
 
   getConnectedWallet(): Wallet | null {
