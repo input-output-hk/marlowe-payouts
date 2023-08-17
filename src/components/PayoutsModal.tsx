b180f08bdeecd2cbb48d316324c6ef485e222e6d
Larry Adames <adames.larry@gmail.com>
2023-08-17 12:20:49 -0400

    PLT-7034 center modal

--- src/components/PayoutsModal.tsx
+++ src/components/PayoutsModal.tsx
@@ -17,34 +17,52 @@ interface PayoutsModalProps {
 const PayoutsModal: React.FC<PayoutsModalProps> = ({ showModal, closeModal, payoutsToBePaidIds, payouts, handleWithdrawals }) => {
   const payoutsToBePaid = payouts.filter(payout => payoutsToBePaidIds.includes(payout.id));
   return (
-    <div className={`modal ${showModal ? 'show' : ''}`} tabIndex={-1} style={{ display: showModal ? 'block' : 'none' }}>
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title">Withdraw Payouts</h5>
-            <button type="button" className="close" onClick={closeModal}>
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div className="modal-body">
-            <p>Summary of payouts to be paid:</p>
-            <ul>
-              {payoutsToBePaid.map((payout, index) => (
-                <li key={index}>{payout.name}: {payout.amount.toString()} lovelace</li>
-              ))}
-            </ul>
-          </div>
-          <div className="modal-footer">
-            <button type="button" className="btn btn-secondary" onClick={closeModal}>
-              Close
-            </button>
-            <button type="button" className="btn btn-primary" onClick={handleWithdrawals}>
-              Confirm Withdrawal
-            </button>
+    <>
+      <div className={`modal ${showModal ? 'show' : ''}`} tabIndex={-1} style={{ display: showModal ? 'block' : 'none' }}>
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Confirm Withdrawal</h5>
+              <button type="button" className="close btn btn-outline-secondary" onClick={closeModal}>
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <div className='col-12'>
+                <p>Summary of payouts to be paid:</p>
+                <ul>
+                  {payoutsToBePaid.map((payout, index) => (
+                    <li key={index}>{payout.name}: {payout.amount.toString()} lovelace</li>
+                  ))}
+                </ul>
+              </div>
+              <div className='divider'></div>
+              <div className='col-12'>
+                Will transfer to
+              </div>
+              <div className='divider'></div>
+            </div>
+            <div className="modal-footer">
+              <div className='container'>
+                <div className='row'>
+                  <div className='col'>
+                    <button type="button" className="btn btn-outline-secondary w-100" onClick={closeModal}>
+                      Cancel
+                    </button>
+                  </div>
+                  <div className='col'>
+                    <button type="button" className="btn btn-primary w-100" onClick={handleWithdrawals}>
+                      Confirm
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {showModal && <div className="modal-backdrop show"></div>}
+    </>
   );
 };
 
