3c5279607cea8cfa1296bd32b1d0b72e9ad1cd38
zeme <lorenzo.calegari@iohk.io>
2023-10-24 12:40:23 +0200

    Added HydraJobs

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -5,5 +5,9 @@
     devShells.default = repoRoot.nix.shell;
 
     packages.marlowe-payouts = repoRoot.nix.marlowe-payouts;
+
+    hydraJobs.devShells.default = repoRoot.nix.shell;
+    hydraJobs.marlowe-payouts = repoRoot.nix.marlowe-payouts;
+    hydraJobs.required = lib.iogx.mkHydraRequiredJob { };
   }
 ]
